dd1b05d1c335d684519bccf09a60f348402b4341
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-30 19:43:38 +0100

    podglad zadania

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
+	event.preventDefault();
 	const clickedElement = this;
 
     /* remove class 'active' from all article links */
@@ -13,21 +13,21 @@ const titleClickHandler = function(event){
 	}
 
     /* add class 'active' to the clicked link */
-	console.log('clickedElement:', clickedElement);
 	clickedElement.classList.add('active');
 
     /* remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll('.posts .active');
-	console.log(activeArticles)
 	for(let activeArticle of activeArticles){
 		activeArticle.classList.remove('active');
 	}
     /* get 'href' attribute from the clicked link */
-    
+    const articleSelector = clickedElement.getAttribute('href');
+
     /* find the correct article using the selector (value of 'href' attribute) */
-    
+    const targetArticle = document.querySelector(articleSelector);
+
     /* add class 'active' to the correct article */
-    
+    targetArticle.classList.add('active');
 }
     
 const links = document.querySelectorAll('.titles a');
