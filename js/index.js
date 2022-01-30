c70deabb487fe040b11c334a34edfc5ce574fb87
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-30 19:21:17 +0100

    2

--- js/index.js
+++ js/index.js
@@ -4,14 +4,24 @@
 
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
-	console.log(event);
-       
+	const clickedElement = this;
+
     /* remove class 'active' from all article links */
-    
+	const activeLinks = document.querySelectorAll('.titles a.active');
+	for(let activeLink of activeLinks){
+		activeLink.classList.remove('active');
+	}
+
     /* add class 'active' to the clicked link */
-    
+	console.log('clickedElement:', clickedElement);
+	clickedElement.classList.add('active');
+
     /* remove class 'active' from all articles */
-    
+	const activeArticles = document.querySelectorAll('.posts .active');
+	console.log(activeArticles)
+	for(let activeArticle of activeArticles){
+		activeArticle.classList.remove('active');
+	}
     /* get 'href' attribute from the clicked link */
     
     /* find the correct article using the selector (value of 'href' attribute) */
