df01529ee1a3a947d7da8c68ffebf0034a9f5d11
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-31 17:20:48 +0100

    koniec modułu 6.4

--- js/index.js
+++ js/index.js
@@ -1,8 +1,50 @@
 'use strict';
+/*------------------- CONSTANTS -------------------*/
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
 
+/*----------------- LEFT COLUMN -------------------*/
 
-const titleClickHandler = function(event){
+const generateTitleLinks = () => {
+    console.log('Generate title links działa.');
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log(titleList);
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    for(let article of articles) {
+
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+
+        /* find the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+        const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+        console.log(linkHTML);
+
+        /* insert link into titleList */
+        titleList.insertAdjacentHTML("beforeend", linkHTML);
+    }
+
+}
+generateTitleLinks();
+
+
+/*----------- Articles CENTER column -----------------*/
+
+const links = document.querySelectorAll('.titles a');
+
+const titleClickHandler = function(event) {
+    console.log('Klikanie działa.');
 	event.preventDefault();
 	const clickedElement = this;
 
@@ -29,10 +71,8 @@ const titleClickHandler = function(event){
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
 }
-    
-const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-	link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
 }
 
