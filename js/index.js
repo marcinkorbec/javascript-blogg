ee32deab25b3f4c9c757887d7ded5db07387a370
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-19 18:44:11 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -130,7 +130,8 @@ function tagClickHandler(event){
 
     /* make a new constant "tag" and extract tag from the "href" constant */
     const tag =  href.replace('#tag-', '');
-	console.log(href)
+	console.log(href);
+
     /* find all tag links with class active */
     const tagLinks = document.querySelectorAll()
 
