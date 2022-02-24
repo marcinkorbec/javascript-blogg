2769e9cc67f335cbb59ac4519b7101e06d08c9d5
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-24 23:15:57 +0100

    add changes

--- js/index.js
+++ js/index.js
@@ -140,39 +140,22 @@ function generateAuthors() {
 generateAuthors();
 
 function authorClickHandler(event){
-
-	/* prevent default action for this event */
 	event.preventDefault();
-
-	/* make new constant named "clickedElement" and give it the value of "this" */
 	const clickedElement = this;
-
-	/* make a new constant "href" and read the attribute "href" of the clicked element */
 	const href = clickedElement.getAttribute('href');
-
-	/* make a new constant "authors" and extract author from the "href" constant */
 	const authors = href.replace('#author-', '');
-
-	/* find all author links with class active */
 	const authorLinks = document.querySelectorAll('.active[href^="#author-"]');
 	console.log(authorLinks)
-	/* START LOOP: for each active author link */
+
 	for (let activeAuthorLink of authorLinks){
-
-		/* remove class active */
 		activeAuthorLink.classList.remove('active');
-
-		/* END LOOP: for each active author link */
 	}
-
-	/* find all autor links with "href" attribute equal to the "href" constant */
 	const equalLinks = document.querySelectorAll('a[href="' + href + '"]');
 
 	for (let equalLink of equalLinks){
 		equalLink.classList.add('active');
 	}
 
-	/* execute function "generateTitleLinks" with article selector as argument */
 	generateTitleLinks('[data-author="' + authors + '"]');
 }
 
